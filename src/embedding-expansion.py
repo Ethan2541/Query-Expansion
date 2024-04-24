e8672542bd1e3cc419e373c2ff744798f02ed299
Ethan2541 <eluong41@gmail.com>
2024-04-24 11:48:03 +0200

    SPARQL

--- src/embedding-expansion.py
+++ src/embedding-expansion.py
@@ -1,11 +1,53 @@
+# Fetch DBpedia dump
+# Train the embeddings on DBpedia dump
+# For each heading, add the 3 most similar embeddings (before or after?). Same goes for the tree
+
 from gensim.models import Word2Vec
+from SPARQLWrapper import SPARQLWrapper, JSON
 import numpy as np
 
-data = None
-model = Word2Vec(sentences=data, vector_size=200, window=10, sample=1e-3, min_count=0, sg=1)
-model.save("entity_word_embeddings.model")
 
-def tree_embedding(headings):
+sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+sparql.setReturnFormat(JSON)
+
+sparql.setQuery("""
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX dcat: <http://www.w3.org/ns/dcat#>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX dcv: <https://dataid.dbpedia.org/databus-cv#>
+    PREFIX databus: <https://dataid.dbpedia.org/databus#>
+    SELECT ?file WHERE
+    {
+        GRAPH ?g
+        {
+            ?dataset databus:artifact <https://databus.dbpedia.org/dbpedia/text/long-abstracts> .
+            {
+                ?distribution dct:hasVersion ?version {
+                    SELECT (?v as ?version) { 
+                        GRAPH ?g2 { 
+                            ?dataset databus:artifact <https://databus.dbpedia.org/dbpedia/text/long-abstracts> . 
+                            ?dataset dct:hasVersion ?v . 
+                        }
+                    } ORDER BY DESC (STR(?version)) LIMIT 1 
+                }
+            }
+            ?dataset dcat:distribution ?distribution .
+            ?distribution databus:file ?file .
+        }
+    }
+""")
+
+
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+print(results)
+
+# data = None
+# model = Word2Vec(sentences=data, vector_size=200, window=10, sample=1e-3, min_count=0, sg=1)
+# model.save("entity_word_embeddings.model")
+
+def global_embedding(headings):
     # Load the model
     model = Word2Vec.load("entity_word_embeddings.model")
     # Get the embeddings for all the headings
--- src/relevance-expansion.py
+++ src/relevance-expansion.py
@@ -1,2 +1,3 @@
 # This file should implement:
+# Rocchio algorithm for relevance modeling
 # A relevance score query/document
